d6daf09170525f7dd0ea67fb5ae7e5573f06f982
maliming <malimings@gmail.com>
2018-06-15 08:45:14 +0800

    Add project files.

---
+++ src/QA.Application/MultiTenancy/ITenantAppService.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using QA.MultiTenancy.Dto;
+
+namespace QA.MultiTenancy
+{
+    public interface ITenantAppService : IAsyncCrudAppService<TenantDto, int, PagedResultRequestDto, CreateTenantDto, TenantDto>
+    {
+    }
+}
